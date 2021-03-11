f63f0a6980f3c8a673171ba46e48908c26b5e7ca
JOE-LAPTOP1\Joe <jsphnelson5@gmail.com>
2021-03-11 12:14:02 -0700

    check username and password

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import Cookie
 app = Flask(__name__, template_folder='templates')
 
+
+#THis is my home page
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Login')
+    return render_template('index.html', title='Welcome')
 
-@app.route('/login')
-    return render_template('dashboard.html')
+#this is the login handler: checks if my username and password are correct or not
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    response = ''
+    if username == 'joey':
+        response += 'Username is correct. <br>'
+    else:
+        response += 'Username is incorrect. Please try again. <br>'
+    if password == '1234':
+        response += 'Password is correct. <br>'
+    else:
+        response += 'Password is incorrect. Please try again. <br>'
+    return response
+
+
+#   return render_template('dashboard.html')
+
+
 if __name__ == '__main__':
    app.run()
